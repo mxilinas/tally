43f224f11b3b6968a39ef102ad61212750d2b4e5
mxilinas <michaelxilinas@gmail.com>
2024-01-16 16:44:19 -0800

    Accounted for last word in the file.

--- src/countwords.c
+++ src/countwords.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /* Function Prototypes */
 int getWord(FILE *, char *);
 Entry *lookup(char *, Entry **);
@@ -10,23 +9,26 @@ Entry *concat(Entry *, Entry *);
 Entry *createEntry(Entry *, int, char *);
 unsigned int hash(char *);
 
-
-/* Generate a dictionary of tallies from a given file */
-void countWords(FILE *in, Entry *hashmap[]) {
+/* Add a new entry to the dictionary. */
+void addEntry(char *word, Entry *hashmap[]) {
   Entry *entry;
-  char word[MAX_WORD_LEN];
-
-  while (getWord(in, word) != EOF) {
-
-    if ((entry = lookup(word, hashmap))) {
-      ++(entry->count);
-      continue;
-    }
-
-    unsigned int hashCode = hash(word);
+  unsigned int hashCode = hash(word);
+  if ((entry = lookup(word, hashmap))) {
+    ++(entry->count);
+  } else {
     if ((entry = hashmap[hashCode]))
       concat(entry, createEntry(NULL, 1, word));
     else
       hashmap[hashCode] = createEntry(NULL, 1, word);
   }
+}
+
+/* Generate a dictionary of tallies from a given file */
+void countWords(FILE *in, Entry *hashmap[]) {
+  char word[MAX_WORD_LEN];
+
+  while (getWord(in, word) != EOF)
+    addEntry(word, hashmap);
+
+  addEntry(word, hashmap);
 }
